9cc7d0a716d3cf0863db860b22e7ebc85053e880
Mudau Mukona <158811188+Mukona2@users.noreply.github.com>
2025-07-23 22:23:24 +0200

    Add files via upload

---
+++ Week 6/Project Files/config.js
@@ -0,0 +1,12 @@
+
+//  (!localStorage.getItem("apiKey")) {
+function checkApiKey() {
+    if (!localStorage.getItem("apiKey")) {
+        window.open("enter-api-key.html", "_self");
+    }
+    return localStorage.getItem("apiKey");
+}
+
+let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
+let apiKey = checkApiKey();
+
